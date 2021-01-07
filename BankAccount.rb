e539a1e3a8018da9f7af3bdcbb50fb3c683e4ce0
kmullen444 <kmullen444@gmail.com>
2021-01-06 18:04:26 -0800

    Created a small banking program that uses public methods to show balance and withdraws while keeping the users pin stored in a privte method.

---
+++ BankAccount.rb
@@ -0,0 +1,41 @@
+class Account 
+  attr_reader :name, :balance
+
+  def initialize(name,balance=100)
+    @name = name 
+    @balance = balance
+  end
+  
+  private 
+
+    def pin 
+      @pin = 1234
+    end
+
+    def pin_error
+      "Access denied: incorrect PIN."
+    end
+
+    public
+
+    def display_balance(pin_number)
+      if pin_number = pin
+        puts "Balance: $#{@balance}."
+      else
+        puts pin_error
+      end
+    end
+
+    def withdraw(pin_number,amount)
+      if pin_number = pin && amount < balance
+        @balance -= amount
+        puts "Withdrew #{amount}. New balance: $#{balance}"
+      elsif pin_number = pin && amount > balance
+        puts "Insuffent funds."
+      else
+        puts pin_error
+      end
+    end
+end
+
+checking_account = Account.new("Kevin", 23_000_000)
